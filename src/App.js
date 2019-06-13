f19a2dafba851a3d4faae9364805484bd3c19312
Candice Marx <candice.marx@hotmail.com>
2019-06-12 20:51:01 -0600

    began setup of lists

--- src/App.js
+++ src/App.js
@@ -1,12 +1,26 @@
-import React from 'react';
+import React, { Component } from 'react';
+import List from './components/todos/List';
+import TodoForm from './components/todos/TodoForm';
+
+class App extends Component {
+  state = { 
+    todos: [
+      { id: 1, name: 'Learn Rails', complete: true },
+      { id: 2, name: 'Learn React', complete: false },
+      { id: 3, name: 'Learn Router', complete: false },
+    ]
+  }
 
 
-function App() {
+render () {
+  const { todos } = this.state
   return (
     <div>
-      
+      <TodoForm />
+      <List name='Todo List' items={todos}/>
     </div>
   );
+}
 }
 
 export default App;
